97dce4a71cc4765548260e0a6709d3f73683da72
richard.fica <richard.fica@gmail.com>
2025-06-15 22:04:42 +0000

    cambio en dashboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,4 +1,4 @@
 jsx
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/
+import { useAuth } from '../context/AuthContext';
